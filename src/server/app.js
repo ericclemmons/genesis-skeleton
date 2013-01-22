9b2a7fc6c2e1427c5b500a4ab83905d66158dcd5
Kevin Whitley <kevin3503@gmail.com>
2013-01-22 15:03:52 -0600

    Adds api module

--- src/server/app.js
+++ src/server/app.js
@@ -5,6 +5,7 @@
 
 var express   = require('express');
 var path      = require('path');
+var api       = require('./lib/api');
 var app       = module.exports = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -15,6 +16,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('change this value to something unique'));
 app.use(express.cookieSession());
+app.use(api);
 app.use(express.static(path.join(__dirname, '../../dist')));
 app.use(express.compress());
 app.use(app.router);
@@ -22,7 +24,3 @@ app.use(app.router);
 if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/api/package', function(req, res) {
-  res.send(require('../../package.json'));
-});
