b6588bb8c0e8131ca14855dfa5f19c0e0729b1ee
Kevin Whitley <kevin3503@gmail.com>
2013-01-22 13:22:03 -0600

    Add "/fail" route to errors lib for 500 testing.

--- src/server/lib/errors/index.js
+++ src/server/lib/errors/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app     = module.exports = express();
 
+app.get('/fail', function(req, res, next) {
+  console.log('attempting to trigger 500...');
+  var a = b[d]; // breakage
+
+  next();
+});
+
 app.get('*', function(req, res) {
   console.log('404 ERROR:', req.url);
   res.redirect('/#' + req.url);
