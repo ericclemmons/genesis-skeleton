1990ecb0767f03c74c47d80407417cc99c46c4bc
Eric Clemmons <eric@smarterspam.com>
2013-02-24 15:57:04 -0600

    Remove favicon middleware

--- src/server/app.js
+++ src/server/app.js
@@ -10,7 +10,6 @@ var app       = module.exports = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
