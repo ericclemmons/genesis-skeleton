518435d866df85cf4cfab7f8b19e51f38d4debac
Eric Clemmons <eric@smarterspam.com>
2013-07-05 16:37:35 -0500

    Initial cleanup for testing

---
+++ client/app/test/unit/columnSpec.js
@@ -0,0 +1,5 @@
+describe("column", function() {
+  it('should have bootstrap column classes', function() {
+
+  });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -3,9 +3,12 @@ basePath = '';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'build/app/scripts/**/*.js',
+  'bower_components/modernizr/modernizr.js',
+  'bower_components/jquery/jquery.js',
+  'bower_components/lodash/dist/lodash.js',
+  'bower_components/backbone/backbone.js',
+  'bower_components/react/react.js',
+  'build/app/scripts/**/all.js',
   'build/app/test/unit/**/*Spec.js'
 ];
 
@@ -15,6 +18,6 @@ runnerPort      = 9100;
 colors          = true;
 logLevel        = LOG_INFO;         // LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 autoWatch       = false;            // Watch for file changes
-browsers        = [ 'PhantomJS' ];  // Chrome, ChromeCanary, Firefox, Opera, Safari (only Mac), PhantomJS, IE (only Windows)
+browsers        = [ ];  // Chrome, ChromeCanary, Firefox, Opera, Safari (only Mac), PhantomJS, IE (only Windows)
 captureTimeout  = 120 * 1000;
 singleRun       = true;             // Exit upon completion
